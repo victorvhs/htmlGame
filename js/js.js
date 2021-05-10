9f71876a458a2d982bede29503bb5468397f08db
Victor Reis <victor.h.s.reis@gmail.com>
2021-05-10 17:13:12 -0300

    shoot

--- js/js.js
+++ js/js.js
@@ -22,14 +22,10 @@ const TECLA = {
 jogo.pressionou = []
 let velocidade = 5;
 let posicaoY = parseInt(Math.random() * 334)
+let podeAtirar=true
 
-$(document).keydown(function(e){
-	jogo.pressionou[e.which] = true
-	});
-
-	$(document).keyup(function(e){
-       jogo.pressionou[e.which] = false
-	});
+$(document).keydown(function(e){jogo.pressionou[e.which] = true})
+	$(document).keyup(function(e){jogo.pressionou[e.which] = false})
 
 function loop() {
 	moveFundo()
@@ -37,8 +33,8 @@ function loop() {
 	moveInimigo1()
 	moveInimigo2()
 	moveAmigo()
+	colisao()
 }
-
 
 function moveFundo(){
 	esquerda = parseInt($("#fundoGame").css("background-position"))
@@ -48,7 +44,6 @@ function moveFundo(){
 function moveJogador() {
 	
 	if (jogo.pressionou[TECLA.W]) {
-
 		const topo = parseInt($("#jogador").css("top"))
 		$("#jogador").css("top",topo-10)
 		if (topo<=0) {
@@ -56,8 +51,7 @@ function moveJogador() {
 		}
 	}
 	
-	if (jogo.pressionou[TECLA.S]) {
-		
+	if (jogo.pressionou[TECLA.S]) {	
 		var topo = parseInt($("#jogador").css("top"))
 		$("#jogador").css("top",topo+10)
 		if (topo>=434) {	
@@ -65,13 +59,11 @@ function moveJogador() {
 				
 		}	
 	}
-	
 	if (jogo.pressionou[TECLA.D]) {
-		
-		//Chama fun��o Disparo	
+		disparo() 	
 	}
+} 
 
-	} 
 function moveInimigo1() {
 	posicaoX = parseInt($("#inimigo1").css("left"))
 	$("#inimigo1").css("left",posicaoX-velocidade)
@@ -90,6 +82,7 @@ function moveInimigo2() {
 		$("#inimigo2").css("left",775)
 	}
 }
+
 function moveAmigo() {
 	posicaoX = parseInt($("#amigo").css("left"))
 	$("#amigo").css("left",posicaoX+1)			
@@ -97,3 +90,39 @@ function moveAmigo() {
 			$("#amigo").css("left",0);	
 		}
 }
+function disparo() {
+	if (podeAtirar==true) {
+		podeAtirar=false;
+		topo = parseInt($("#jogador").css("top"))
+		posicaoX= parseInt($("#jogador").css("left"))
+		tiroX = posicaoX + 190;
+		topoTiro=topo+37;
+		$("#fundoGame").append("<div id='disparo'></div")
+		$("#disparo").css("top",topoTiro)
+		$("#disparo").css("left",tiroX)
+		var tempoDisparo=window.setInterval(executaDisparo, 30)
+	}
+	function executaDisparo() {
+	    posicaoX = parseInt($("#disparo").css("left"))
+	    $("#disparo").css("left",posicaoX+15)
+			if (posicaoX>900) {	
+				window.clearInterval(tempoDisparo)
+				tempoDisparo=null
+				$("#disparo").remove()
+				podeAtirar=true
+			}
+	}
+}
+
+function colisao() {
+	let colisao1 = ($("#jogador").collision($("#inimigo1")))
+		if (colisao1.length>0){ 		
+			inimigo1X = parseInt($("#inimigo1").css("left"));
+			inimigo1Y = parseInt($("#inimigo1").css("top"));
+			explosao1(inimigo1X,inimigo1Y);
+			posicaoY = parseInt(Math.random() * 334);
+			$("#inimigo1").css("left",694);
+			$("#inimigo1").css("top",posicaoY);
+		}
+}
+
